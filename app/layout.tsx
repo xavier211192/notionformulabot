5226c0a1694e83b9ec869c58c78f575a0ad98a3f
Prashanth Xavier <xavier211192@gmail.com>
2025-09-05 22:59:04 +0200

    Analytics enable for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: 'Notion Assist',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-white text-gray-900">{children}</body>
+      <body className="bg-white text-gray-900">
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
